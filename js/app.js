adc5d1bb0fc66117ae86000db7cffdb4055eed2d
neilw22 <neilw22@gmail.com>
2015-04-15 11:46:47 -0400

    Initial setup, random number generated and allows for correct guess to display

--- js/app.js
+++ js/app.js
@@ -12,6 +12,45 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+// Random number on page load
+var hiddenNumber = Math.floor((Math.random() * 100) + 1);
+console.log(hiddenNumber);
+
+function newGame() {
+	hiddenNumber = Math.floor((Math.random() * 100) + 1);
+	$("#userGuess").empty();
+	$("#guessList").empty();
+	$("#feedback").text("Make Your Guess!");
+}
+
+// New Game Button
+
+	$("a.new").click(function(){
+		newGame();
+	});
+
+$("#guessButton").click(function(){
+	var userNumber = $("#userGuess").val();
+	if (userNumber == hiddenNumber) {
+		$("#feedback").text("Correct!");
+	} else {
+		$("#feedback").text("Inorrect!");
+	}
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
 
 
