33c7410a211e4860b8efff73d4b72ce48affa260
neilw22 <neilw22@gmail.com>
2015-04-16 23:18:39 -0400

    Created new game function, and functional correct or incorrect guess function

--- js/app.js
+++ js/app.js
@@ -12,43 +12,45 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	$("a.new").click(function(){
+	newGame();
+	});
+
 // Random number on page load
 var hiddenNumber = Math.floor((Math.random() * 100) + 1);
 console.log(hiddenNumber);
 
-function newGame() {
+function newGame(){
 	hiddenNumber = Math.floor((Math.random() * 100) + 1);
+	console.log(hiddenNumber);
+
 	$("#userGuess").empty();
 	$("#guessList").empty();
+	$("#userGuess").val('');
 	$("#feedback").text("Make Your Guess!");
+
 }
 
-// New Game Button
-
-	$("a.new").click(function(){
-		newGame();
-	});
-
-$("#guessButton").click(function(){
-	var userNumber = $("#userGuess").val();
-	if (userNumber == hiddenNumber) {
-		$("#feedback").text("Correct!");
-	} else {
-		$("#feedback").text("Inorrect!");
-	}
-
-});
-
-
-
-
+	$("#guessButton").click(function(g) {
+		g.preventDefault();
+		var userNumber = $("#userGuess").val();
+		console.log(userNumber);
+		if (isNaN(userNumber)) {
+			$("#feedback").text("Please enter a number!");
+			$("#userGuess").val('');
+		}
+		else if (userNumber == hiddenNumber) {
+			$("#feedback").text("Correct!");
+			$("#userGuess").val('');
+		} else {
+			$("#feedback").text("Incorrect!");
+			$("#userGuess").val('');
+			$("#guessList").append("<li />"+userNumber);
+		}
+	})
 
 
-
-
-
-
-
+// newGame();
 
 
 });
